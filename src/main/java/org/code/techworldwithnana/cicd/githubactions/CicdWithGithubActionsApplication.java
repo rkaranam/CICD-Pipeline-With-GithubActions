665ffb66d1e16938bab6a1651710be1ae74c04c5
Raja Sekhar K <karanam.rajasekhar@gmail.com>
2025-02-16 18:58:27 +0530

    Initial code setup

---
+++ src/main/java/org/code/techworldwithnana/cicd/githubactions/CicdWithGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package org.code.techworldwithnana.cicd.githubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdWithGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdWithGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/code/techworldwithnana/cicd/githubactions/CicdWithGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.code.techworldwithnana.cicd.githubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdWithGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
